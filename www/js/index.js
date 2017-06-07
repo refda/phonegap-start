a11b8c45b42c558c66fdbd15da9574e81bbeb21e
refda <s@merq.de>
2017-06-07 08:40:56 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        window.open("https://kraftstoffbilliger.de", '_blank', 'toolbar=no,location=no');
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
